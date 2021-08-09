b750462b3e242a8411cd48e813f98578eb422e03
Frederick <FrederickRauh@hotmail.com>
2021-08-09 19:49:51 +0200

    test

--- scripts/SvmScript.py
+++ scripts/SvmScript.py
@@ -1,13 +1,13 @@
 import logging
 
-from datetime import datetime
+# from datetime import datetime
+#
+# import frontend.frontend
+# from backend.svm import SVM
+#
+# from utils import util
 
-import frontend.frontend
-from backend.svm import SVM
-
-from utils import util
-
-svm = SVM()
+# svm = SVM()
 
 if __name__ == '__main__':
     while(True):
@@ -19,15 +19,15 @@ if __name__ == '__main__':
     # logger = logging.getLogger()
     # logger.disabled = True
 
-    start_time = datetime.now()
-    logging.info(f"Version SVM :{start_time}")
-    logging.info(f"feature version: {config.FEATURE_TYPE}, mfcc count: {config.N_MFCC}")
-    # preparation phase
-    frontend.frontend.feature_extraction_for_n_speaker(speaker_ids=IDS.SPEAKER_IDS, create_dataframe=True,
-                                                       feature_type=config.FEATURE_TYPE)
-    # training phase
-    svm.train(speaker_ids=IDS.SPEAKER_IDS, feature_type=config.FEATURE_TYPE)
-    # prediction phase
-    svm.predict_n_speakers(speaker_ids=IDS.SPEAKER_IDS, feature_type=config.FEATURE_TYPE)
-
-    logging.info(f"----------------------------------------------------------{util.get_duration(start_time)}")
+    # start_time = datetime.now()
+    # logging.info(f"Version SVM :{start_time}")
+    # logging.info(f"feature version: {config.FEATURE_TYPE}, mfcc count: {config.N_MFCC}")
+    # # preparation phase
+    # frontend.frontend.feature_extraction_for_n_speaker(speaker_ids=IDS.SPEAKER_IDS, create_dataframe=True,
+    #                                                    feature_type=config.FEATURE_TYPE)
+    # # training phase
+    # svm.train(speaker_ids=IDS.SPEAKER_IDS, feature_type=config.FEATURE_TYPE)
+    # # prediction phase
+    # svm.predict_n_speakers(speaker_ids=IDS.SPEAKER_IDS, feature_type=config.FEATURE_TYPE)
+    #
+    # logging.info(f"----------------------------------------------------------{util.get_duration(start_time)}")
